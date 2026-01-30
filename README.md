de24bc3a75fc8478427dff9cb5a2c0ae21765110
Rafael Rubén Jorge González <rrjorgegz@gmail.com>
2026-01-30 13:03:03 -0500

    fix :  change img

--- README.md
+++ README.md
@@ -4,11 +4,6 @@
 
 This template helps you build a portfolio using Astro, Tailwind CSS, and Shadcn UI.
 
-## Design
-
-![View 1](public/View1.png)
-![View 2](public/View2.png)
-
 ## Inspiration
 
 This project was inspired by several outstanding portfolios. Here are some that served as references:
--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,23 @@
+// astro.config.ts
 import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
 export default defineConfig({
   site: process.env.CI
     ? 'https://astro-shadcn-ui-template.vercel.app'
     : 'http://localhost:4321',
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({ applyBaseStyles: false })],
+  vite: {
+    build: {
+      chunkSizeWarningLimit: 1000, // opcional: subir el límite
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            timezone: ['moment', 'moment-timezone'],
+          },
+        },
+      },
+    },
+  },
 })
